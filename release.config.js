333045c8a8ac3632ed75b7ba289c7a011b3f1832
Wendelin <wendelin.steiner@gmail.com>
2021-12-20 15:27:20 +0100

    added code formatting

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/2Up1Down/github-actions-cicd",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
-    ]
-}
+  branches: 'master',
+  repositoryUrl: 'https://github.com/2Up1Down/github-actions-cicd',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
