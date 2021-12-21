7916f8545edd709adb3a173473f80b5ccd9822af
Wendelin <wendelin.steiner@gmail.com>
2021-12-21 14:43:30 +0100

    test workflow with typo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
         </a>
         <p>hello again</p>
         <p>added some feature</p>
